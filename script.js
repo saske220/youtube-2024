5e100d8401000778a999626bc45d1102fbff7b0e
saske220 <146696108+saske220@users.noreply.github.com>
2024-04-26 10:25:17 +0900

    ka
    ka

---
+++ script.js
@@ -0,0 +1,11 @@
+const dengen = documnet.querySelector(".Red");
+
+const tv = document.querySelector(".video");
+
+dengen.addEventListener("click", () => {
+  if (tv.classList.contains("on")) {
+    tv.classList.toggle("off");
+  } else {
+    tv.classList.toggle("on");
+  }
+});
